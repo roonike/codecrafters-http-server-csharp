b6c1353254b51844853195a9098bd52c2bfa3505
Fitzberth Powell Channer <fitzberth.powellchanner@ucr.ac.cr>
2024-06-22 23:32:44 -0600

    pass 1st stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
+using system.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+socket.send("HTTP/1.1 200 OK\r\n\r\n");
